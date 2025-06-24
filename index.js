4aca6ad5aef94bafab369ef561cfa6a43691c790
ismailtaher <ismail.taher@ymail.com>
2025-06-24 17:08:01 +0500

    add async/await readFile

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
+// Commonjs module
+
 //
 // readFile
 //
 
-// ES Module
-/* import { readFile } from "node:fs";
-
-readFile("./files/starter.txt", "utf8", (err, data) => {
-  if (err) throw err;
-  console.log(data);
-}); */
-
-// Commonjs module
-const fs = require("fs");
+/* const fs = require("fs"); */
 const path = require("path"); // to make path of the file not be hardcoded
 
-fs.readFile(
+/* fs.readFile(
   path.join(__dirname, "files", "starter.txt"),
   "utf8",
   (err, data) => {
@@ -22,28 +15,28 @@ fs.readFile(
     console.log(data.toString());
   }
 );
-
-console.log("Hello!");
+ */
+/* console.log("Hello!"); */
 
 // catch error uncaughtException
 
-process.on("uncaughtException", (err) => {
+/* process.on("uncaughtException", (err) => {
   console.error(`There was an uncaught error: ${err}`);
   process.exit(1);
-});
+}); */
 
 //
 // writeFile
 //
 
-fs.writeFile(
+/* fs.writeFile(
   path.join(__dirname, "files", "reply.txt"),
   "Nice to meet you",
   (err) => {
     if (err) throw err;
     console.log("write complete");
   }
-);
+); */
 
 //
 // appendFile
@@ -51,14 +44,14 @@ fs.writeFile(
 
 // creating a file
 
-fs.appendFile(
+/* fs.appendFile(
   path.join(__dirname, "files", "test.txt"),
   "Testing text",
   (err) => {
     if (err) throw err;
     console.log("Append complete");
   }
-);
+); */
 
 // updating a file
 
@@ -86,7 +79,7 @@ fs.appendFile(
 
 // Note: All these operations are async but we are controlling when they come into action by nesting them in the callbacks of operations we need them to occur after
 
-fs.writeFile(
+/* fs.writeFile(
   path.join(__dirname, "files", "replyWithAppend.txt"),
   "Nice to meet you",
   (err) => {
@@ -111,4 +104,26 @@ fs.writeFile(
       }
     );
   }
-);
+); */
+
+// The code above is good but it is called the callback hell in JS. Because we are putting one operation inside the callback of another operation. So we are able to controll the flow of operations but at the same time we can avoid this callback hell using async/await in Node.js as well! Code down below
+
+//
+// async/await
+//
+
+const fsPromises = require("fs").promises;
+
+const fileOps = async () => {
+  try {
+    const data = await fsPromises.readFile(
+      path.join(__dirname, "files", "starter.txt"),
+      "utf8"
+    );
+    console.log(data);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+fileOps();
